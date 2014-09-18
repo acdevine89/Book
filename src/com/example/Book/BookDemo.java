1725646f735b54e40d2184e48bffb90a59f2abfc
Annie Devine <anniedevine@Annies-MacBook-Pro.local>
2014-09-18 14:29:01 -0400

    Final version

--- src/com/example/Book/BookDemo.java
+++ src/com/example/Book/BookDemo.java
@@ -9,7 +9,6 @@ public class BookDemo {
 
     public static void main(String[] args) {
         Book myBook = new Book();
-
         myBook.setTitle("The Lord of the Rings: The Fellowship of the Ring");
         myBook.setAuthor("J.R.R. Tolkien");
         myBook.setNumberOfPages(426);
